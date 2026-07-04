dd3bccafb05aa8452efec7f9d5f41d07fdcae2f2
dancoder-engineer <japaneseforgaming@gmail.com>
2026-07-04 10:57:44 -0400

    Halfway through the Speedywagon Foundation exercise.

--- current.cpp
+++ current.cpp
@@ -22,9 +22,16 @@ int uv_light_heuristic(std::vector<int>* data_array) {
 }
 
 bool connection_check(pillar_men_sensor* sensor){
-    return sensor == nullptr;
+    return sensor != nullptr;
 }
 
+int activity_counter(pillar_men_sensor* sensor_array, int capacity){
+    int sum = 0;
+    for(int i = 0; i < capacity; i++){
+        sum += (sensor_array + i)->activity;
+    }
+    return sum;
+}
 
 
 
@@ -32,6 +39,12 @@ int main(){
 
     pillar_men_sensor* sensor{nullptr};
     bool isConnected = connection_check(sensor);
+    std::cout << isConnected << "\n";
+
+    pillar_men_sensor sensor_array[3] = {{0}, {101}, {22}};
+    int totalActivity = activity_counter(sensor_array, 3);
+    // totalActivity => 123
+    std::cout << totalActivity << "\n";
  
     return 0;
 }
--- pushgit.bat
+++ pushgit.bat
@@ -1,3 +1,5 @@
 git add .
-git commit -am "Finished the lasagna exercise."
+REM Halfway through the ppp exercise.
+REM Finished the ppp exercise.
+git commit -am "Halfway through the Speedywagon Foundation exercise."
 git push
