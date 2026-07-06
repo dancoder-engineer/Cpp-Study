7449979f62bedddaafb052cb1e953bf92257dd89
dancoder-engineer <japaneseforgaming@gmail.com>
2026-07-06 07:16:02 -0400

    Finished the clocks exercise.

--- current.cpp
+++ current.cpp
@@ -2,72 +2,37 @@
 #include <string>
 #include <vector>
 
-namespace date_independent {
-
-    class clock{
-
-        public:
-
-        long time() const{
-            return currentMinute;
-        }
-
-        static clock at(long hour, long minute){
-            return clock(hour * 60 + minute);
-        }
-
-        clock plus(int minutes){
-            int minute = normalize(currentMinute + minutes);
-            return at(minute / 60, minute % 60);
-        }
-
-        operator std::string() const{
-            long hour = currentMinute / 60;
-            long minute = currentMinute % 60;
-            return doubleDigitString(hour) + ":" + doubleDigitString(minute);
-        }
-
-        bool operator==(const clock& other) const{
-            return currentMinute == other.time();
-        }
-
-        bool operator!=(const clock& other) const{
-            return currentMinute != other.time();
-        }
 
 
+std::string noOfBottles(int num){
+    if(num == 0) { return "No more bottles"; }
+    std::string numString = std::to_string(num);
+    std::string howManyBottles = numString + "bottle";
+    if(num > 1) { howManyBottles += "s"; }
+    return howManyBottles;
+}
 
-        private:
+std::string verse(int num){
+    //99 bottles of beer on the wall, 99 bottles of beer.
+    //Take one down and pass it around, 98 bottles of beer on the wall.
+    std::string line1 = noOfBottles(num) + " bottles of beer on the wall, " + noOfBottles(num) +  " bottles of beer.\n";
+    std::string line2 = "";
+    if(num - 1 == 0) {
+        line2 = "Go to the store and buy some more, 99 bottles of beer on the wall.\n";
+    }
+    else{
+        line2 = "Take one down and pass it around, " + noOfBottles(num - 1) + " bottles of beer on the wall.\n";
 
-        long currentMinute;
-        
-
-        clock(long minute){
-            currentMinute = normalize(minute);
-        }
-
-        static long normalize(long minute){
-            minute = minute % 1440;
-            if(minute < 0) { minute += 1440; }
-            return minute;
-        }
-
-        static std::string doubleDigitString(long num){
-            std::string numString = std::to_string(num);
-            return num >= 10 ? numString : "0" + numString; 
-        }
-        
-
-    };
-
+    }
 
 }
 
+    
+
 int main(){
 
-    date_independent::clock testClock = date_independent::clock::at(0, -15);
-    std::cout <<  static_cast<std::string>(testClock);
-
+  
+    std::cout << verse(5) << "\n";
  
     return 0;
 }
