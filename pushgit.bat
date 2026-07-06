0401a0573609549933acf7b4234bba51815337b2
dancoder-engineer <japaneseforgaming@gmail.com>
2026-07-06 07:51:25 -0400

    Halfway through the anagrams exercise.

--- pushgit.bat
+++ pushgit.bat
@@ -1,5 +1,5 @@
 git add .
 REM Halfway through the ppp exercise.
 REM Finished the ppp exercise.
-git commit -am "Halfway through the bottles of beer exercise."
+git commit -am "Halfway through the anagrams exercise."
 git push
