dfbfc8d55ed3d42f5bbe21bca202f20b5df4d4fc
dancoder-engineer <japaneseforgaming@gmail.com>
2026-07-06 08:05:26 -0400

    Finished the anagrams exercise.

--- pushgit.bat
+++ pushgit.bat
@@ -1,5 +1,5 @@
 git add .
 REM Halfway through the ppp exercise.
 REM Finished the ppp exercise.
-git commit -am "Halfway through the anagrams exercise."
+git commit -am "Finished the anagrams exercise."
 git push
