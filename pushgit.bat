6a7ca45eb193bfc8411d66b439c4bbf9cd4170ad
dancoder-engineer <japaneseforgaming@gmail.com>
2026-07-08 08:32:30 -0400

    Added notes on linked lists.

--- pushgit.bat
+++ pushgit.bat
@@ -1,5 +1,5 @@
 git add .
 REM Halfway through the ppp exercise.
 REM Finished the ppp exercise.
-git commit -am "Finished the string compression exercise."
+git commit -am "Added notes on linked lists."
 git push
